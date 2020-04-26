3d5c851cc4218f6f34c3524c9e3fe5f1211dfe73
jayaramanhari <jayaramanhari@gmail.com>
2020-04-26 09:12:53 +0530

    Illegal state failure fix - try 1

--- docker-life-cycle-with-JavaAndSpringBootApp/src/test/java/com/books/chapters/serviceMesh/springboot/controller/InvestorControllerTest.java
+++ docker-life-cycle-with-JavaAndSpringBootApp/src/test/java/com/books/chapters/serviceMesh/springboot/controller/InvestorControllerTest.java
@@ -7,16 +7,15 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import com.books.chapters.serviceMesh.springboot.controller.InvestorController;
 import com.books.chapters.serviceMesh.springboot.service.InvestorService;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @WebMvcTest(value = InvestorController.class, secure = false)
 public class InvestorControllerTest {
 
